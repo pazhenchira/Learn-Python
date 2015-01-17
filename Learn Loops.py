48d2edbbd4055c3f289e3aecfeb6e75ae9167e03
Mathew John <mathewj@mathewj.com>
2015-01-17 11:10:48 -0800

    Python - variables & loops

---
+++ Learn Loops.py
@@ -0,0 +1,13 @@
+# find the table of values for y = 37x - 5
+
+print("Building a table of values for the equation y = 37x - 5")
+x = 0
+while x < 10:
+    y = 37*x - 5
+    print("x y = ", x, y)
+     x = x + 1
+
+print("all done");
+
+
+
---
+++ Learn Variables.py
@@ -0,0 +1,17 @@
+# find the table of values for y = 37x - 5
+
+x = 0
+y = 37*x - 5
+print("x y = ", x, y)
+
+x = 1
+y = 37*x - 5
+print("x y = ", x, y)
+
+x = 2
+y = 37*x - 5
+print("x y = ", x, y)
+
+x = 3
+y = 37*x - 5
+print("x y = ", x, y)
